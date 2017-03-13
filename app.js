1d1664782bc1719b676151ee12907c54b1561191
Simon Powell <simonpo@microsoft.com>
2017-03-13 16:50:00 -0700

    hacking with authbot code

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 // Set up our requirements
-var restify = require('restify')
+var express = require('express')
+	, restify = require('restify')
   , builder = require('botbuilder')
   , passport = require('passport')
-  , SoundCloudStrategy = require('passport-soundcloud').Strategy; 
-var SC = require ('node-soundcloud');
-const util = require('util');
+  , SoundCloudStrategy = require('passport-soundcloud').Strategy
+	, SC = require ('node-soundcloud')
+	, util = require('util');
 const expressSession = require('express-session');
-
-// Sometimes handy for debugging
-// const util = require('util');
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -24,96 +22,351 @@ var connector = new builder.ChatConnector({
 })
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
-
-// Add LUIS recognizer
-var recognizer = new builder.LuisRecognizer(process.env.MY_LUIS_MODEL);
-var intents = new builder.IntentDialog({ recognizers: [recognizer] });
-
-// Create bot dialogs
-bot.dialog('/', intents);
-intents.matches(/^change name/i, [
-	function (session) {
-		session.beginDialog("/profile");
-	},
-	function (session, results) {
-		session.send("Ok... Changed your name to %s", session.privateConversationData.name);
-	}
-]);
-intents.matches('Greeting', builder.DialogAction.send('Hello.'));
-intents.matches('Login', [
-	function (session, args, next) {
-		if (!session.privateConversationData.name) {
-			session.beginDialog("/profile");
-		} else {
-			next();
-		}
-	},
-
-	function (session, results) {
-		session.send("Hello %s!", session.privateConversationData.name);
-	}
-]);
-intents.matches('Help', builder.DialogAction.send("Basic help information goes here."));
-intents.matches('AboutTheBot', builder.DialogAction.send("I'm a chat bot, built by Simon Powell to do things with music."));
-intents.onDefault(builder.DialogAction.send("I'm sorry I didn't understand. I don't know a lot yet."));
-
-bot.dialog("/profile", [
-	function (session) {
-		builder.Prompts.text(session, "Hi! What is your name?");
-	},
-
-	function (session, results) {
-		if (results.response.match(/login/gi)) {
-      SC.init({
-        id: process.env.MY_SC_ID,
-        secret: process.env.MY_SC_SECRET,
-        uri: process.env.MY_SC_URI
-      })
-
-      var url = SC.getConnectUrl();
-      console.log('URL is %s', url)
-
-			session.send(new builder.Message(session).addAttachment(
-				new builder.SigninCard(session)
-					.text("Authenticate with Soundcloud")
-					.button("Sign-In", url))
-			);
-		} else {
-			session.privateConversationData.name = results.response;
-			session.endDialog();
-		}
-	}
-]);
-
-// web interface
 server.get('/', restify.serveStatic({
  directory: __dirname,
  default: '/index.html',
 }));
 
-server.get('/api/oauthcallback/', function (req, res, next) {  
-   console.log('OAuth Callback');
-   var code = req.query.code;
-   console.log(code);
+// Add LUIS recognizer
+// var recognizer = new builder.LuisRecognizer(process.env.MY_LUIS_MODEL);
+// var intents = new builder.IntentDialog({ recognizers: [recognizer] });
 
-SC.init({
-  id: process.env.MY_SC_ID,
-  secret: process.env.MY_SC_SECRET,
-  uri: process.env.MY_SC_URI
-})
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+server.use(expressSession({ secret: 'keyboard cat', resave: true, saveUninitialized: false }));
+server.use(passport.initialize());
 
-   SC.authorize(code, function(err, accessToken) {
-   if ( err ) {
-     throw err;
-   } else {
-     // Client is now authorized and able to make API calls 
-    console.log('access token: %s', accessToken);
-    bot.beginDialog(address, "/oauth-success", tokens);
+server.get('/login', function (req, res, next) {
+  passport.authenticate('passport-soundcloud', { failureRedirect: '/login', customState: req.query.address, resourceURL: process.env.MICROSOFT_RESOURCE }, function (err, user, info) {
+    console.log('login');
+    if (err) {
+      console.log(err);
+      return next(err);
     }
+    if (!user) {
+      return res.redirect('/login');
+    }
+    req.logIn(user, function (err) {
+      if (err) {
+        return next(err);
+      } else {
+        return res.send('Welcome ' + req.user.displayName);
+      }
+    });
+  })(req, res, next);
+});
+
+server.get('/api/oauthcallback/',
+  passport.authenticate('passport-soundcloud', { failureRedirect: '/login' }),
+  (req, res) => {
+    console.log('OAuthCallback');
+    console.log(req);
+    const address = JSON.parse(req.query.state);
+    const magicCode = crypto.randomBytes(4).toString('hex');
+    const messageData = { magicCode: magicCode, accessToken: req.user.accessToken, refreshToken: req.user.refreshToken, userId: address.user.id, name: req.user.displayName, email: req.user.preferred_username };
+    
+    var continueMsg = new builder.Message().address(address).text(JSON.stringify(messageData));
+    console.log(continueMsg.toMessage());
+
+    bot.receive(continueMsg.toMessage());
+    res.send('Welcome ' + req.user.displayName + '! Please copy this number and paste it back to your chat so your authentication can complete: ' + magicCode);
+});
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(id, done) {
+  done(null, id);
+});
+
+// Use the v2 endpoint (applications configured by apps.dev.microsoft.com)
+// For passport-azure-ad v2.0.0, had to set realm = 'common' to ensure authbot works on azure app service
+var realm = 'simonpo-soundbot.azurewebsites.net'; // AZUREAD_APP_REALM; 
+var AUTHBOT_CALLBACKHOST = 'simonpo-soundbot.azurewebsites.net';
+let oidStrategyv2 = {
+  redirectUrl: AUTHBOT_CALLBACKHOST + '/api/oauthcallback',
+  realm: realm,
+  clientID: process.env.MY_APP_ID,
+  clientSecret: process.env.MY_APP_PASSWORD,
+  identityMetadata: 'https://login.microsoftonline.com/' + realm + '/v2.0/.well-known/openid-configuration',
+  skipUserProfile: false,
+  validateIssuer: false,
+  //allowHttpForRedirectUrl: true,
+  responseType: 'code',
+  responseMode: 'query',
+  scope:['email', 'profile', 'offline_access', 'https://outlook.office.com/mail.read'],
+  passReqToCallback: true
+};
+
+// Use the v1 endpoint (applications configured by manage.windowsazure.com)
+// This works against Azure AD
+let oidStrategyv1 = {
+  redirectUrl: AUTHBOT_CALLBACKHOST +'/api/oauthcallback',
+  realm: realm,
+  clientID: process.env.MY_APP_ID,
+  clientSecret: process.env.MY_APP_PASSWORD,
+  validateIssuer: false,
+  //allowHttpForRedirectUrl: true,
+  oidcIssuer: undefined,
+  identityMetadata: 'https://login.microsoftonline.com/' + realm + '/.well-known/openid-configuration',
+  skipUserProfile: true,
+  responseType: 'code',
+  responseMode: 'query',
+  passReqToCallback: true
+};
+
+// let strategy = null;
+let strategy = oidStrategyv2;
+
+passport.use(new SoundCloudStrategy(strategy,
+  (req, iss, sub, profile, accessToken, refreshToken, done) => {
+    if (!profile.displayName) {
+      return done(new Error("No oid found"), null);
+    }
+    // asynchronous verification, for effect...
+    process.nextTick(() => {
+      profile.accessToken = accessToken;
+      profile.refreshToken = refreshToken;
+      return done(null, profile);
+    });
+  }
+));
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+function login(session) {
+  // Generate signin link
+  const address = session.message.address;
+
+  // TODO: Encrypt the address string
+  const link = AUTHBOT_CALLBACKHOST + '/login?address=' + querystring.escape(JSON.stringify(address));
+  
+
+  var msg = new builder.Message(session) 
+    .attachments([ 
+        new builder.SigninCard(session) 
+            .text("Please click this link to sign in first.") 
+            .button("signin", link) 
+    ]); 
+  session.send(msg);
+  builder.Prompts.text(session, "You must first sign into your account.");
+}
+
+bot.dialog('signin', [
+  (session, results) => {
+    console.log('signin callback: ' + results);
+    session.endDialog();
+  }
+]);
+
+bot.dialog('/', [
+  (session, args, next) => {
+    if (!(session.userData.userName && session.userData.accessToken && session.userData.refreshToken)) {
+      session.send("Welcome! This bot retrieves the latest email for you after you login.");
+      session.beginDialog('signinPrompt');
+    } else {
+      next();
+    }
+  },
+  (session, results, next) => {
+    if (session.userData.userName && session.userData.accessToken && session.userData.refreshToken) {
+      // They're logged in
+      builder.Prompts.text(session, "Welcome " + session.userData.userName + "! You are currently logged in. To get the latest email, type 'email'. To quit, type 'quit'. To log out, type 'logout'. ");
+    } else {
+      session.endConversation("Goodbye.");
+    }
+  },
+  (session, results, next) => {
+    var resp = results.response;
+    if (resp === 'email') {
+      session.beginDialog('workPrompt');
+    } else if (resp === 'quit') {
+      session.endConversation("Goodbye.");
+    } else if (resp === 'logout') {
+      session.userData.loginData = null;
+      session.userData.userName = null;
+      session.userData.accessToken = null;
+      session.userData.refreshToken = null;
+      session.endConversation("You have logged out. Goodbye.");
+    } else {
+      next();
+    }
+  },
+  (session, results) => {
+    session.replaceDialog('/');
+  }
+]);
+
+bot.dialog('workPrompt', [
+  (session) => {
+    getUserLatestEmail(session.userData.accessToken,
+        function (requestError, result) {
+          if (result && result.value && result.value.length > 0) {
+            const responseMessage = 'Your latest email is: "' + result.value[0].Subject + '"';
+            session.send(responseMessage);
+            builder.Prompts.confirm(session, "Retrieve the latest email again?");
+          }else{
+            console.log('no user returned');
+            if(requestError){
+              console.log('requestError');
+              console.error(requestError);
+              // Get a new valid access token with refresh token
+              getAccessTokenWithRefreshToken(session.userData.refreshToken, (err, body, res) => {
+
+                if (err || body.error) {
+                  console.log(err);
+                  session.send("Error while getting a new access token. Please try logout and login again. Error: " + err);
+                  session.endDialog();
+                }else{
+                  session.userData.accessToken = body.accessToken;
+                  getUserLatestEmail(session.userData.accessToken,
+                    function (requestError, result) {
+                      if (result && result.value && result.value.length > 0) {
+                        const responseMessage = 'Your latest email is: "' + result.value[0].Subject + '"';
+                        session.send(responseMessage);
+                        builder.Prompts.confirm(session, "Retrieve the latest email again?");
+                      }
+                    }
+                  );
+                }
+                
+              });
+            }
+          }
+        }
+      );
+  },
+  (session, results) => {
+    var prompt = results.response;
+    if (prompt) {
+      session.replaceDialog('workPrompt');
+    } else {
+      session.endDialog();
+    }
+  }
+]);
+
+bot.dialog('signinPrompt', [
+  (session, args) => {
+    if (args && args.invalid) {
+      // Re-prompt the user to click the link
+      builder.Prompts.text(session, "please click the signin link.");
+    } else {
+      login(session);
+    }
+  },
+  (session, results) => {
+    //resuming
+    session.userData.loginData = JSON.parse(results.response);
+    if (session.userData.loginData && session.userData.loginData.magicCode && session.userData.loginData.accessToken) {
+      session.beginDialog('validateCode');
+    } else {
+      session.replaceDialog('signinPrompt', { invalid: true });
+    }
+  },
+  (session, results) => {
+    if (results.response) {
+      //code validated
+      session.userData.userName = session.userData.loginData.name;
+      session.endDialogWithResult({ response: true });
+    } else {
+      session.endDialogWithResult({ response: false });
+    }
+  }
+]);
+
+bot.dialog('validateCode', [
+  (session) => {
+    builder.Prompts.text(session, "Please enter the code you received or type 'quit' to end. ");
+  },
+  (session, results) => {
+    const code = results.response;
+    if (code === 'quit') {
+      session.endDialogWithResult({ response: false });
+    } else {
+      if (code === session.userData.loginData.magicCode) {
+        // Authenticated, save
+        session.userData.accessToken = session.userData.loginData.accessToken;
+        session.userData.refreshToken = session.userData.loginData.refreshToken;
+
+        session.endDialogWithResult({ response: true });
+      } else {
+        session.send("hmm... Looks like that was an invalid code. Please try again.");
+        session.replaceDialog('validateCode');
+      }
+    }
+  }
+]);
+
+function getAccessTokenWithRefreshToken(refreshToken, callback){
+  console.log("getAccessTokenWithRefreshToken");
+  var data = 'grant_type=refresh_token' 
+        + '&refresh_token=' + refreshToken
+        + '&client_id=' + process.env.MY_APP_ID
+        + '&client_secret=' + encodeURIComponent(process.env.MY_APP_PASSWORD) 
+
+  var options = {
+      method: 'POST',
+      url: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
+      body: data,
+      json: true,
+      headers: { 'Content-Type' : 'application/x-www-form-urlencoded' }
+  };
+
+  request(options, function (err, res, body) {
+      if (err) return callback(err, body, res);
+      if (parseInt(res.statusCode / 100, 10) !== 2) {
+          if (body.error) {
+              return callback(new Error(res.statusCode + ': ' + (body.error.message || body.error)), body, res);
+          }
+          if (!body.access_token) {
+              return callback(new Error(res.statusCode + ': refreshToken error'), body, res);
+          }
+          return callback(null, body, res);
+      }
+      callback(null, {
+          accessToken: body.access_token,
+          refreshToken: body.refresh_token
+      }, res);
+  }); 
+}
+
+function getUserLatestEmail(accessToken, callback) {
+  var options = {
+    host: 'outlook.office.com', //https://outlook.office.com/api/v2.0/me/messages
+    path: '/api/v2.0/me/MailFolders/Inbox/messages?$top=1',
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: 'Bearer ' + accessToken
+    }
+  };
+  https.get(options, function (response) {
+    var body = '';
+    response.on('data', function (d) {
+      body += d;
+    });
+    response.on('end', function () {
+      var error;
+      if (response.statusCode === 200) {
+        callback(null, JSON.parse(body));
+      } else {
+        error = new Error();
+        error.code = response.statusCode;
+        console.log(response.statusMessage);
+        error.message = response.statusMessage;
+        // The error body sometimes includes an empty space
+        // before the first character, remove it or it causes an error.
+        // body = body.trim();
+        // console.log(body);
+        // error = body;
+        callback(response.statusMessage, null);
+      }
+    });
+  }).on('error', function (e) {
+    callback(e, null);
   });
-
-bot.dialog("/oauth-success", function (session, tokens) {  
-  session.send('Authenticated succesfully!');
-});
-
-});
+}
